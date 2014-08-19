5972251b757e7303190deadf99f87e159afa7ee7
BoonsNaibot <deguzouwa@yahoo.com>
2014-08-19 00:02:27 -0500

    update

--- WeakMethod.pxd
+++ WeakMethod.pxd
@@ -1,13 +1,13 @@
 from cpython.ref cimport PyObject
 
 cdef extern from "Python.h":
-    object PyWeakref_NewRef(PyObject *ob, PyObject *callback)
+    PyObject* PyWeakref_NewRef(PyObject *ob, PyObject *callback)
     PyObject* PyWeakref_GetObject(PyObject *ref)
-    object PyObject_GetAttr(PyObject *o, PyObject *name)
+    PyObject* PyObject_GetAttr(PyObject *o, PyObject *name)
     void Py_XINCREF(PyObject *o)
     
 cdef class WeakMethod(object):
     cdef int id
     cdef object _obj
     cdef str _func
-    cdef PyObject* _get_object(self, object x)
+    cdef PyObject* _get_object(self, PyObject *x)
--- WeakMethod.pyx
+++ WeakMethod.pyx
@@ -6,18 +6,18 @@ cdef class WeakMethod(object):
         self._func = method.__name__
         if method.__self__ is not None:
             # bound method
-            self._obj = PyWeakref_NewRef(method.im_self, None)
+            self._obj = <object>PyWeakref_NewRef(<PyObject*>method.im_self, <PyObject*>None)
         else:
             # unbound method
-            self._obj = PyWeakref_NewRef(method.im_class, None)
+            self._obj = <object>PyWeakref_NewRef(<PyObject*>method.im_class, <PyObject*>None)
 
-    cdef PyObject* _get_object(self, object x):
+    cdef PyObject* _get_object(self, PyObject *x):
         cdef PyObject *y = PyWeakref_GetObject(x)
         Py_XINCREF(y)
         return y
 
     def __call__(self, *args):
-        return PyObject_GetAttr(self._get_object(self._obj), <PyObject*>self._func)(*args)
+        return <object>(PyObject_GetAttr(self._get_object(<PyObject*>self._obj), <PyObject*>self._func))(*args)
 
     def __richcmp__(self, object other, int op):
         if op == 2:
@@ -27,4 +27,4 @@ cdef class WeakMethod(object):
 
     property is_dead:
         def bint __get__(WeakMethod self):
-            return <object>PyWeakref_GetObject(<PyObject*>self._obj) is None
+            return <object>(PyWeakref_GetObject(<PyObject*>self._obj)) is None
