45ef806af879acb1dc7ec189330e171a7f1b9ff3
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-04 16:35:46 -0500

    Update civyobject.h

--- civyobject.h
+++ civyobject.h
@@ -7,3 +7,14 @@ typedef struct {
     PyObject *load_event;
     PyGreenlet *main_loop;   /* "Class Method" */
 } CVObject;
+
+
+static PyObject* CVObject_new(PyTypeObject *type, PyObject *args, PyObject *kwargs);
+static int CVObject_init(CVObject *self, PyObject *args, PyObject *kwargs);
+static void CVObject_dealloc(CVObject *self);
+
+static PyTypeObject CVObject_Type;
+
+#ifndef CVOBJECTMODINIT_FUNC
+#define CVOBJECTMODINIT_FUNC void
+#endif
