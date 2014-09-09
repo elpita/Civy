607758eab928ea4832bdcd2f3e19e78b5f010856
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-09 11:58:38 -0500

    Create q.h

---
+++ q.h
@@ -0,0 +1,17 @@
+typedef struct _QEntry {
+    QEntry *previous;
+    QEntry *next;
+    } QEntry;
+
+
+struct Q {
+    QEntry *head;
+    QEntry *next;
+    };
+
+
+Q* Q_new(void);
+void Q_dealloc(Q *q);
+int Q_is_empty(Q *q);
+void Q_push(Q *self);
+QEntry* Q_pop(Q *self);
