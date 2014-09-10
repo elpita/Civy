315ed9d8ad1f668efd90705c14f7e3f8a435d585
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-10 14:30:58 -0500

    Update civycontext.c

--- civycontext.c
+++ civycontext.c
@@ -26,7 +26,6 @@ CVContext* CVContext_new(PyObject *event_handler)
     context->loop = PyGreenlet_New(CVContext_loop, NULL);
     PyObject *capsule = PyCapsule_New((void *)context, NULL, NULL);
     PyGreenlet_Switch(context->loop, capsule);
-    Py_XDECREF(capsule);
     context->handler = PyWeakref_NewRef(event_handler, NULL);
     return context;
     }
