0628826b3e609e6681070267076b1c5bebc27ab5
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-13 13:47:19 -0500

    Fork & Wait objects
    Initialization still needs to be done

--- civycontext.h
+++ civycontext.h
@@ -2,27 +2,39 @@
 #define civycontext_included
 
 #include "q.h"
-#include <Python.h>
 #include "greenlet.h"
 
-PyObject *CVAbdicate;
-PyObject *CVYield;
+
+typedef struct {
+    PyObject_HEAD
+} cv_ForkSentinel;
+static PyTypeObject cv_ForkSentinelType;
+
+
+typedef cv_ForkSentinel {
+    PyObject *data;
+} cv_WaitSentinel;
+static PyTypeObject cv_WaitSentinelType;
+
 
 typedef _QEntry _CVContext {
     PyObject *handler;
     PyGreenlet *spawn;
     CVContext *parent;
     Q *pipeline;
-    } CVContext;
+} CVContext;
 
 
 typedef _QEntry whatever {
     PyGreenlet *cvthread;
-    };
+};
 
 
 int CVContext_dealloc(CVContext *self);
 void CVThreads_push(CVContext *self, PyGreenlet *new_entry);
 PyGreenlet* CVThreads_pop(CVContext *self);
 
+
+#define Fork_Check(op)    PyObject_TypeCheck(op, &cv_ForkSentinelType)
+#define Wait_Check(op)    PyObject_TypeCheck(op, &cv_WaitSentinelType)
 #endif
