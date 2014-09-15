e9b4535ba4f28c1e3e6952f48b7d9ece6d04de3d
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-14 22:38:31 -0500

    Update civyobject.h

--- civyobject.h
+++ civyobject.h
@@ -1,23 +1,18 @@
 #ifndef civyobject_included
 #define civyobject_included
 
-#include "civycontext.h"
+#include "civyprocess.h"
 
-typedef struct {
-    PyObject_HEAD
-    Q *cvprocesses;
-    PyGreenlet *exec;
-    PyGreenlet *main_loop;   /* "Class Method" */
-} CVObject;
-
-
-static PyObject* CVObject_new(PyTypeObject *type, PyObject *args, PyObject *kwargs);
-static int CVObject_init(CVObject *self, PyObject *args, PyObject *kwargs);
-static void CVObject_dealloc(CVObject *self);
-
+typedef struct _cvobject *CVObject;
 static PyTypeObject CVObject_Type;
 
-#ifndef CVOBJECTMODINIT_FUNC
-#define CVOBJECTMODINIT_FUNC void
-#endif
+
+static int check_process(CVProcess process);
+static PyObject* CVObject_exec(PyObject *self);
+static PyObject* CVObject_new(PyTypeObject *type, PyObject *args, PyObject *kwargs);
+static int CVObject_init(CVObject self, PyObject *args, PyObject *kwargs);
+static void CVObject_dealloc(CVObject self);
+
+
+
 #endif
