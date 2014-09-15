457ac5642805b8290b399f2d72feb03339b18a7a
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-15 13:27:14 -0500

    Update civyobject.h

--- civyobject.h
+++ civyobject.h
@@ -8,6 +8,7 @@ static PyTypeObject CVObject_Type;
 
 
 static int check_process(CVProcess process);
+static PyObject* CVObject_spawn(CVObject self, PyObject *callback, PyObject *args);
 static PyObject* CVObject_exec(PyObject *self);
 static PyObject* CVObject_new(PyTypeObject *type, PyObject *args, PyObject *kwargs);
 static int CVObject_init(CVObject self, PyObject *args, PyObject *kwargs);
