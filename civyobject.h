99e70b2828891796256010d17acd12ee9ac7096e
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-17 19:25:20 -0500

    Update civyobject.h

--- civyobject.h
+++ civyobject.h
@@ -4,7 +4,6 @@
 #include "civyprocess.h"
 
 typedef struct _cvobject *CVObject;
-static PyTypeObject CVObject_Type;
 
 
 static int check_process(CVProcess process);
@@ -13,7 +12,5 @@ static PyObject* CVObject_exec(PyObject *self);
 static PyObject* CVObject_new(PyTypeObject *type, PyObject *args, PyObject *kwargs);
 static int CVObject_init(CVObject self, PyObject *args, PyObject *kwargs);
 static void CVObject_dealloc(CVObject self);
-
-
 
 #endif
