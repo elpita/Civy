6b21b9e46b7892c3ad14bdc2d624514fbfd24159
BoonsNaibot <deguzouwa@yahoo.com>
2014-09-22 19:56:45 -0500

    Update civyprocess.h

--- civyprocess.h
+++ civyprocess.h
@@ -13,16 +13,4 @@ static CVProcess CVProcess_new(PyObject *event_handler);
 static int CVProcess_dealloc(CVProcess self);
 static int CVProcess_push_thread(CVProcess self, PyGreenlet *data);
 static PyGreenlet* CVProcess_pop_thread(CVProcess self);
-
-static void **IMPORT_civyprocess = NULL;
-
-#define DOT_CVPROCESS_NEW                       0
-#define DOT_CVPROCESS_DEALLOC                   1
-#define DOT_CVPROCESS_PUSH_THREAD               2
-#define DOT_CVPROCESS_POP_THREAD                3
-
-#define civyprocess_dot_CVProcess_new           (*(_CVProcess *)IMPORT_civyprocess[DOT_CVPROCESS_NEW])
-#define civyprocess_dot_CVProcess_dealloc       (*(int)IMPORT_civyprocess[DOT_CVPROCESS_DEALLOC])
-#define civyprocess_dot_CVProcess_push_thread   (*(int *)IMPORT_civyprocess[DOT_CVPROCESS_PUSH_THREAD])
-#define civyprocess_dot_CVProcess_pop_thread    (*(PyGreenlet *)IMPORT_civyprocess[DOT_CVPROCESS_POP_THREAD])
 #endif
