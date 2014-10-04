66e4815539a4b27669d77d9d421cdf6f8ba44e74
BoonsNaibot <deguzouwa@yahoo.com>
2014-10-04 11:28:46 -0500

    Moved `check_cvprocess` to `civyprocess.c`.

--- civyprocess.h
+++ civyprocess.h
@@ -10,6 +10,7 @@ typedef struct _cvprocess *CVProcess;
 
 static PyObject* CVProcess_loop(PyObject *capsule);
 static CVProcess CVProcess_new(PyObject *event_handler);
+static int check_cvprocess(CVProcess process);
 static void CVProcess_dealloc(CVProcess self);
 static int CVProcess_push_thread(CVProcess self, PyGreenlet *data);
 static PyGreenlet* CVProcess_pop_thread(CVProcess self);
