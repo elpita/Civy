233f6c5748d7f8d7efffe1914502efadfc77bb7c
BoonsNaibot <deguzouwa@yahoo.com>
2014-10-10 12:19:20 -0500

    Make `Queue_dealloc` a macro.

--- q.h
+++ q.h
@@ -6,9 +6,9 @@ typedef struct _queue *Q;
 
 
 static Q Queue_new(void);
-static void Queue_dealloc(Q self);
 static void Queue_push(Q self, QEntry new_entry);
 static void Queue_prepend(Q self, QEntry new_entry);
 static QEntry Queue_pop(Q self);
 #define Q_IS_EMPTY(q) (q->head == NULL)
+#define Queue_dealloc(q) free(q)
 #endif
