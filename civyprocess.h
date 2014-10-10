a8c23ad8caf959eccdeafb4fb05cdab4b45d6ccb
BoonsNaibot <deguzouwa@yahoo.com>
2014-10-10 14:19:05 -0500

    Update civyprocess.h

--- civyprocess.h
+++ civyprocess.h
@@ -12,5 +12,5 @@ static PyObject* CVProcess_loop(PyObject *capsule);
 static CVProcess CVProcess_new(PyObject *event_handler);
 static void CVProcess_dealloc(CVProcess self);
 static int CVProcess_push_thread(CVProcess self, PyGreenlet *data);
-static PyGreenlet* CVProcess_pop_thread(CVProcess self);
+static PyGreenlet* CVProcess_pop_thread(Q pipeline);
 #endif
