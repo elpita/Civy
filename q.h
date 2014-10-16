4c408d0df2dc649df4cc2e911187e6dcdf090afe
BoonsNaibot <deguzouwa@yahoo.com>
2014-10-16 14:05:37 -0500

    `Queue_init` is now a macro. -`Queue_dealloc`

--- q.h
+++ q.h
@@ -5,10 +5,9 @@ typedef struct _queueentry *QEntry;
 typedef struct _queue *Q;
 
 
-static void Queue_init(Q q);
 static void Queue_push(Q self, QEntry new_entry);
 static void Queue_prepend(Q self, QEntry new_entry);
 static QEntry Queue_pop(Q self);
 #define Q_IS_EMPTY(q) (q->head == NULL)
-#define Queue_dealloc(q) free(q)
+#define Queue_init(q) (q->head = q->tail = NULL);
 #endif
