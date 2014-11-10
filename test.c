5c72553e248944cf1f0e51a5fd40994f5b1cb1bf
BoonsNaibot <deguzouwa@yahoo.com>
2014-11-10 15:13:20 -0600

    Update test.c

--- test.c
+++ test.c
@@ -1,25 +1,56 @@
-static enum { STARTED, ACTIVE, YIELDED, DEAD } state;
+static enum { STARTED, ACTIVE, DEAD } state;
+
+
+static int pop()
+{
+    _process = _pop(_current->pipeline);
+    if (_process == NULL) {
+        return 0;
+    }
+    return 1;
+}
+
+
+static void process_routine(void)
+{
+    switch(setjmp(_process->jmp)) {
+        case 1:
+            roll(); //nullify currrent thread
+            push_routine();
+            break;
+        default:
+            switch(_process->state) {
+                case STARTED:
+                    _process->state = ACTIVE;
+                    return;
+                    while(pop_routine()) {
+                        unroll(); //reinstate saved thread
+                        result = PyEval_CallObjectWithKeywords(_routine->func, args, kwargs);
+                        case ACTIVE:;
+                    }
+            }
+    }
+    longjmp(_main->jmp, 1);
+}
 
 
 static void actor_routine(void)
 {
-    switch(_current->state) {
-        case STARTED:
-            switch(setjmp(_current->jmp)) {
-                case 1:
-                    unroll();
-                    get_sp(something);
-                    longjmp(_main->jmp);
-            }
-            _current->state = YIELDED;
-            return;
-            while(1) {
-                _current->state = ACTIVE;
-                case ACTIVE:
-                    _process = pop(_current->pipeline);
-                    roll(_process->local_stack);
-                    call(_process);
-                case YIELDED:;
+    switch(setjmp(_current->jmp)) {
+        case 1:
+            roll();
+            break;
+        default:
+            switch(_current->state) {
+                case STARTED:
+                    _current->state = ACTIVE;
+                    return;
+                    while(pop()) {
+                        unroll();
+                        process_routine();
+                        case ACTIVE:;
+                    }
             }
     }
+    longjmp(_main->jmp, 1);
 }
