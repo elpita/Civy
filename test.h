e839c89970994fe33ae1409bfcddbc477ac476d0
BoonsNaibot <deguzouwa@yahoo.com>
2014-11-22 17:44:54 -0600

    Create test.h

---
+++ test.h
@@ -0,0 +1,10 @@
+#ifndef CIVYMAIN
+#define CIVYMAIN
+
+#if defined(_WIN32) || defined(_WIN64)
+#define sleep(dt) Sleep(1000*dt)
+#endif
+
+static SDL_Event main_event;
+static jmp_buf to_main_loop;
+#endif
