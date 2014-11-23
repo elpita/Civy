3727ecbb6e8b934240bf40bd845df36b8876bfce
BoonsNaibot <deguzouwa@yahoo.com>
2014-11-23 14:38:39 -0600

    Update test.c

--- test.c
+++ test.c
@@ -31,3 +31,57 @@ static void cv_main_loop(void)
     /* Call final function(s) */
     EXIT_CV
 }
+
+
+static int cv_app_init(PyObject *self, PyObject *args, PyObject *kwds)
+{
+    Uint32 mask;
+    Py_ssize_t len;
+    PyObject *obj, *seq;
+    char *cv_a = "CV_AUDIO", *cv_gc = "CV_GAME_CONTROLLER", *cv_j = "CV_JOYSTICK", *cv_ff = "CV_FORCE_FEEDBACK";
+
+    SDL_assert(!SDL_WasInit(SDL_INIT_EVERYTHING));
+    mask = SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS;
+
+    if (!PyArg_ParseTuple(args, "O", &obj)) {
+        return -1;
+    }
+    seq = PySequence_Fast(obj, "expected a sequence of strings.");
+    
+    if (seq == NULL) {
+        return -1;
+    }
+    len = PySequence_Size(seq);
+
+    if (len  < 0) {
+        Py_DECREF(seq);
+        return -1;
+    }
+    for (i = 0; i < len; i++) {
+        str_item = PySequence_Fast_GET_ITEM(seq, i);
+        string = PyString_AsString(str_object);
+
+        if (string == NULL) {
+            Py_DECREF(seq);
+            return -1;
+        }
+        else if (strcmp(string, cv_a) == 0) {
+            mask |= SDL_INIT_AUDIO;
+        }
+        else if (strcmp(string, cv_gc) == 0) {
+            mask |= SDL_INIT_GAMECONTROLLER;
+        }
+        else if (strcmp(string, cv_j) == 0) {
+            mask |= SDL_INIT_JOYSTICK;
+        }
+        else if (strcmp(string, cv_ff) == 0) {
+            mask |= SDL_INIT_HAPTIC;
+        }
+    }
+    if (SDL_Init(mask) < 0) {
+        PyErr_SetString(PyExc_RuntimeError, SDL_GetError());
+        return -1;
+    }
+    tbd_global_app_pointer = self;
+    return 0;
+}
