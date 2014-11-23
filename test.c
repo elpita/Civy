70467413820d4df73498e38e8e025b80af866cf9
BoonsNaibot <deguzouwa@yahoo.com>
2014-11-22 18:20:04 -0600

    Update test.c

--- test.c
+++ test.c
@@ -1,34 +1,33 @@
 #include "test.h"
-#define CV_EVENT_LOOP_START case 0: while(1) {
-#define CV_EVENT_LOOP_END case 1:;}break;
+#define CV_EVENT_LOOP_START switch(setjmp(to_main_loop)) { case 0: while(1) {
+#define CV_EVENT_LOOP_END case 1: ; } break; case -1:
+#define EXIT_CV break; }
 
 
 static void cv_main_loop(void)
 {
-    switch(setjmp(to_main_loop))
-    {
-        CV_EVENT_LOOP_START
-        switch(SDL_PollEvent(&main_event)) {
-            case 0:
-                Py_BEGIN_ALLOW_THREADS
-                sleep(0.02);
-                Py_END_ALLOW_THREADS
-                break;
-            default:
-                switch(main_event.type) {
-                    case DISPATCHED_EVENT:
-                        /* call some nonsense */
-                        break;
-                    default:
-                        /* call some other nonsense */
-                        break;
-                }
-                break;
-        }
-        CV_EVENT_LOOP_END
+    CV_EVENT_LOOP_START
 
-        case -1:
-            /* Call final function(s) */
+    switch(SDL_PollEvent(&main_event)) {
+        case 0:
+            Py_BEGIN_ALLOW_THREADS
+            sleep(0.02);
+            Py_END_ALLOW_THREADS
+            break;
+        default:
+            switch(main_event.type) {
+                case DISPATCHED_EVENT:
+                    /* call some nonsense */
+                    break;
+                default:
+                    /* call some other nonsense */
+                    break;
+            }
             break;
     }
+
+    CV_EVENT_LOOP_END
+
+    /* Call final function(s) */
+    EXIT_CV
 }
