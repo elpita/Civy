c9e35776d7b34934611c5cb8dc857d07d7bc80b4
BoonsNaibot <deguzouwa@yahoo.com>
2014-12-26 14:38:39 -0600

    Update and rename something.c to coroutinestructs.c

---
+++ coroutinestructs.c
@@ -0,0 +1,44 @@
+/* Continuation Object */
+struct _cvargsstruct {
+    PyObject *args;
+    PyObject *kwds;
+};
+
+
+struct _cvcontext {
+    int state;
+    PyObject *passaround;
+    void *vars;
+};
+
+
+typedef struct _cvcontinuation *CVContinuation;
+struct _cvcontinuation {
+    struct _cvcontext context;
+    struct _cvargsstruct argsptr;
+};
+
+
+/* Coroutine Object */
+#ifndef CV_STACK_LENGTH
+#define CV_STACK_LENGTH 4
+#endif /* CV_STACK_LENGTH */
+
+struct _cvstack {
+    struct _cvcontinuation items[CV_STACK_LENGTH];
+    CVContinuation cvstackptr;
+}
+
+
+typedef struct _cvcoroutine *CVCoroutine;
+struct _cvcoroutine {
+    struct _cvstack stack;
+    CVCoroutine parent;
+    /* actor queue ptr */
+};
+
+
+typedef struct _cvactor *CVActor;
+struct _cvactor {
+    
+};
