45764ded4ee36a29641117d3acd4884e2e184352
BoonsNaibot <deguzouwa@yahoo.com>
2014-12-30 16:02:48 -0600

    Create civycontinuation.c

---
+++ civycontinuation.c
@@ -0,0 +1,21 @@
+#include "civycontinuation.h"
+
+struct _cvcontext {
+    int state;
+    PyObject *passaround;
+    void *vars;
+};
+
+
+struct _cvcontinuation {
+    struct _cvcontext context;
+    PyObject *argsptr[3];
+};
+
+
+static cv_dealloc_args(PyObject *args)
+{
+    Py_XDECREF(args[0]);
+    Py_XDECREF(args[1]);
+    Py_XDECREF(args[2]);
+}
