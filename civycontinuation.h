df26514520a72d6fe3dff796c32e637575a2d758
BoonsNaibot <deguzouwa@yahoo.com>
2014-12-30 16:00:23 -0600

    Create civycontinuation.h

---
+++ civycontinuation.h
@@ -0,0 +1,10 @@
+#ifndef CIVYCONTINUATION
+#define CIVYCONTINUATION
+#include "Python.h"
+
+typedef struct _cvcontext *CVContext;
+typedef struct _cvcontinuation *CVContinuation;
+
+static cv_dealloc_args(PyObject *args);
+
+#endif /* CIVYCONTINUATION */
