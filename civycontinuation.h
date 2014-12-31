7c059d7bde3e9ccebb73325924e5761695f7ac20
BoonsNaibot <deguzouwa@yahoo.com>
2014-12-30 20:30:06 -0600

    Update civycontinuation.h

--- civycontinuation.h
+++ civycontinuation.h
@@ -5,6 +5,6 @@
 typedef struct _cvcontext *CVContext;
 typedef struct _cvcontinuation *CVContinuation;
 
-static cv_dealloc_args(PyObject *args);
+static void cv_dealloc_args(PyObject *args);
 
 #endif /* CIVYCONTINUATION */
