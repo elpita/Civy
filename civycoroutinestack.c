f74a2397c51970132ab07396a69f966d75c6abad
BoonsNaibot <deguzouwa@yahoo.com>
2014-12-30 23:04:42 -0600

    Create civycoroutinestack.c

---
+++ civycoroutinestack.c
@@ -0,0 +1,44 @@
+#include "civycoroutinestack.h"
+
+
+struct _cvcostack {
+    CVContinuation s_ptr;
+    struct _cvcontinuation items[CV_STACK_LENGTH];
+}
+
+
+static void cv_init_stack(CVCoStack s)
+{
+    s->s_ptr = s->items;
+}
+
+
+static void cv_dealloc_stack(CVCoStack s)
+{
+    PyObject *arg;
+    CVContinuation c;
+
+    while ((c = cv_costack_pop(s)) != NULL) { //Not ANSI C, but i don't care.
+        cv_dealloc_args(c->argsptr);
+    }
+    //*s = NULL;
+}
+
+
+static void cv_costack_push(CVCoStack s, CVContinuation c)
+{
+    if (s->s_ptr >= (s->items + STACK_SIZE)) {
+       PyErr_SetString(PyExc_RuntimeError, "Overflow in coroutine stack.");
+       /* jump back */
+    }
+    *s->s_ptr++ = *c;
+}
+
+
+static CVContinuation cv_costack_pop(CVCoStack s)
+{
+    if (s->s_ptr == s->items) {
+       return NULL;
+    }
+    return --s->s_ptr;
+}
