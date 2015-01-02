afaba5e5d5dc2d5143e145a5a587a2974c4ac206
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-02 12:23:17 -0600

    -`Q_IS_EMPTY`

--- civyactorqueue.h
+++ civyactorqueue.h
@@ -6,7 +6,6 @@ typedef struct _cvactorqueue *CVActorQ;
 
 static int cv_actor_queue_push(CVActorQ self, CVCoroutine coro);
 static CVCoroutine cv_actor_queue_pop(CVActorQ self);
-#define Q_IS_EMPTY(q) (q->head == NULL)
 #define Queue_init(q) (q->head = q->tail = NULL)
 
 #endif /* CIVYACTORQUEUE */
