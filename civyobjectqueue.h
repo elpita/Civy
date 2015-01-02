b84cc4a049747bacd5df4bf082af0e25a677d437
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-02 15:26:47 -0600

    Update civyobjectqueue.h

--- civyobjectqueue.h
+++ civyobjectqueue.h
@@ -1,11 +1,11 @@
-#ifndef CIVYOBJECTQUEUE
-#define CIVYOBJECTQUEUE
+#ifndef CIVYOBJECTQUEUE_INCLUDED
+#define CIVYOBJECTQUEUE_INCLUDED
 #include "civycoroutine.h"
 
-typedef struct _cvactorqueue *CVObjectQ;
+typedef struct _cvobjectqueue *CVObjectQ;
 
-static int cv_actor_queue_push(CVObjectQ self, CVCoroutine coro);
-static CVCoroutine cv_actor_queue_pop(CVObjectQ self);
+static int cv_object_queue_push(CVObjectQ self, CVCoroutine coro);
+static CVCoroutine cv_object_queue_pop(CVObjectQ self);
 #define Queue_init(q) (q->head = q->tail = NULL)
 
-#endif /* CIVYOBJECTQUEUE */
+#endif /* CIVYOBJECTQUEUE_INCLUDED */
