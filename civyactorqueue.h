c14c64a2132ba18a89130ed73f5ef423b69d6cd9
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-02 12:05:13 -0600

    Update civyactorqueue.h

--- civyactorqueue.h
+++ civyactorqueue.h
@@ -5,9 +5,8 @@
 typedef struct _cvactorqueue *CVActorQ;
 typedef struct _cvactorqueueentry *CVActorQEntry;
 
-static void Queue_push(CVActorQ self, CVActorQEntry new_entry);
-static void Queue_prepend(CVActorQ self, CVActorQEntry new_entry);
-static QEntry Queue_pop(CVActorQ self);
+static int cv_actor_queue_push(CVActorQ self, CVCoroutine coro);
+static CVCoroutine cv_actor_queue_pop(CVActorQ self);
 #define Q_IS_EMPTY(q) (q->head == NULL)
 #define Queue_init(q) (q->head = q->tail = NULL);
 
