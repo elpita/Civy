0dade91745642eeb5632e8e2d2fdc9d4f08b527e
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-02 10:38:39 -0600

    Create civycoroutine.h

---
+++ civycoroutine.h
@@ -0,0 +1,9 @@
+#ifndef CIVYCOROUTINE
+#define CIVYCOROUTINE
+#include "civycoroutinestack.h"
+
+typedef struct _cvcoroutine *CVCoroutine;
+
+static CVCoroutine cv_create_coroutine();
+
+#endif /* CIVYCOROUTINE */
