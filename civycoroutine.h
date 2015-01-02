04203600081282e1af0261f2f250a1a3c9ccf329
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-02 11:27:58 -0600

    Update civycoroutine.h

--- civycoroutine.h
+++ civycoroutine.h
@@ -4,6 +4,7 @@
 
 typedef struct _cvcoroutine *CVCoroutine;
 
-static CVCoroutine cv_create_coroutine();
+static CVCoroutine cv_create_coroutine(PyObject *actor);
+static void cv_dealloc_coroutine(CVCoroutine self);
 
 #endif /* CIVYCOROUTINE */
