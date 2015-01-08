bba7deaf1978af870f580e2e4d164fc16e120742
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-07 18:37:49 -0600

    Update civycoroutine.h

--- civycoroutine.h
+++ civycoroutine.h
@@ -7,4 +7,6 @@ typedef struct _cvcoroutine *CVCoroutine;
 static CVCoroutine cv_create_coroutine(PyObject *actor);
 static void cv_dealloc_coroutine(CVCoroutine self);
 
+#define is_dead(a) ((a == Py_None) || _is_dead(a))
+
 #endif /* CIVYCOROUTINE */
