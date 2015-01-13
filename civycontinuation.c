bc12da3cadd7446d572c126c3c965054f7f4ae51
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-13 11:10:30 -0600

    Update civycontinuation.c

--- civycontinuation.c
+++ civycontinuation.c
@@ -9,8 +9,17 @@ struct _cvcontext {
 struct _cvcontinuation {
     struct _cvcontext context;
     CVCallbackFunc cocall;
+    CVCleanupFunc coclean;
     PyObject *argsptr[3];
 };
+
+
+static void cv_dealloc_continuation(CVContinuation c)
+{
+    CVCleanupFunc cleanup = c->coclean;
+    cv_dealloc_args(c->argsptr);
+    cleanup((CVContext)->vars);
+}
 
 
 static void cv_dealloc_args(PyObject *args)
@@ -18,4 +27,5 @@ static void cv_dealloc_args(PyObject *args)
     Py_XDECREF(args[0]);
     Py_XDECREF(args[1]);
     Py_XDECREF(args[2]);
+    
 }
