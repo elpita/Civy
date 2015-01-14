96a8babe8e3bff57b7e80aca2b8c4b2d596e5c2a
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-14 14:51:41 -0600

    `void cv_costack_push` --> `int cv_costack_push`

--- civycoroutinestack.h
+++ civycoroutinestack.h
@@ -6,8 +6,8 @@ typedef struct _cvcostack *CVCoStack;
 
 static CVCoStack cv_init_costack(CVCoStack s);
 static void cv_dealloc_costack(CVCoStack s);
-static void cv_stack_push(CVCoStack s, CVContinuation c);
-static CVContinuation cv_stack_pop(CVCoStack s);
+static int cv_costack_push(CVCoStack s, CVContinuation c);
+static CVContinuation cv_costack_pop(CVCoStack s);
 
 #ifndef CV_STACK_LENGTH
 #define CV_STACK_LENGTH 128
