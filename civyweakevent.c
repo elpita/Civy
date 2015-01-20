8d369e48579acf5773c7ba1d92f2ccb5d073c67d
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-19 21:45:50 -0600

    Update civyweakevent.c

--- civyweakevent.c
+++ civyweakevent.c
@@ -1,14 +1,14 @@
 typdedef struct _cvweakevent *CVWeakEvent;
 struct _cvweakevent {
-    int id;
     PyObject *obj;
-    PyObject *name;
+    int id;
 }
 
 
 static int CVWeakEvent_init(CVWeakEvent self, PyObject *args, PyObject *kwds)
 {
     PyObject *callback;
+    long int id;
     static char *kwargs[] = {"callback", NULL};
 
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "O", kwargs, &callback)) {
@@ -16,6 +16,7 @@ static int CVWeakEvent_init(CVWeakEvent self, PyObject *args, PyObject *kwds)
     }
     else if (PyFunction_Check(callback)) {
         self->obj = callback;
+        Py_INCREF(callback);
     }
     else {
         PyObject *obj = PyObject_GetAttrString(callback, "__func__");
@@ -25,10 +26,25 @@ static int CVWeakEvent_init(CVWeakEvent self, PyObject *args, PyObject *kwds)
         }
         self->obj = obj;
     }
-    self->id = PyObject_Hash(callback);
+    id = PyObject_Hash(callback);
 
     if (id < 0) {
         Py_DECREF(self->obj);
         return -1;
     }
+    self->id = id;
+    return 0;
+}
+
+
+static PyObject* CVWeakEvent_call(CVWeakEvent self, PyObject *args, PyObject *kwds)
+{
+    s
+}
+
+
+static void CVWeakEvent_dealloc(CVWeakEvent self)
+{
+    Py_DECREF(self->obj);
+    self->ob_type->tp_free((PyObject *)self);
 }
