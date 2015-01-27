be1be7a2d227538a68f17e9b72612ebf54fc0ff8
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-27 13:26:06 -0600

    Update civycoroutine.c

--- civycoroutine.c
+++ civycoroutine.c
@@ -2,56 +2,64 @@
 
 
 typedef struct _cvcorostate {
-    CVCoroutine parent;
+    CVCoroutine *parent;
     PyObject *actor_ptr;
 } CVCoroState;
 
-struct _cvcoroutine {
-    struct _cvstack stack;
-    struct _cvcorostate state;
-};
+
+typedef struct _cvcoroutine {
+    CVStack stack;
+    CVCoroState state;
+} CVCoroutine;
 
 
-static CVCoroutine cv_create_coroutine(PyObject *actor)
+static CVCoroutine* cv_create_coroutine(PyObject *actor)
 {
-    CVCoroutine self = (struct _cvcoroutine *)PyMem_Malloc(sizeof(struct _cvcoroutine));
+    CVStack self_stack;
+    CVCoroState self_state;
+    CVCoroutine *self = (CVCoroutine *)PyMem_Malloc(sizeof(CVCoroutine));
 
     if (self == NULL) {
         return PyErr_NoMemory();
     }
-    self->actor_ptr = PyWeakref_NewRef(actor, NULL);
+    else {
+        PyObject *state_actor_ptr = PyWeakref_NewRef(actor, NULL);
 
-    if (self->actor_ptr == NULL) {
-        PyMem_Free(self);
-        return NULL;
+        if (state_actor_ptr == NULL) {
+            PyMem_Free(self);
+            return NULL;
+        }
+        self_state.actor_ptr = state_actor_ptr;
+        self_state.parent = NULL;
     }
-    cv_init_costack(&self->stack);
-    self->parent = NULL;
+    cv_init_costack(&self_stack);
+    self->stack = self_stack;
+    self->state = self_state;
     return self;
 }
 
 
-static void kill_cvcoroutine(CVCoroutine c)
+static void kill_cvcoroutine(CVCoroutine *c)
 {
     cv_dealloc_costack(&c->stack);
-    Py_DECREF(c->actor_ptr);
+    Py_DECREF((c->state).actor_ptr);
     PyMem_Free(c);
 }
 
 
-static void cv_dealloc_coroutine(CVCoroutine self)
+static void cv_dealloc_coroutine(CVCoroutine *self)
 {
-    CVCoroutine p, c;
+    CVCoroutine *p, *c;
 
-    for(p = self->parent; p != NULL; p = c) {
-        c = p->parent
+    for (p = (self->state).parent; p != NULL; p = c) {
+        c = (p->state).parent;
         kill_cvcoroutine(p);
     }
     kill_cvcoroutine(self);
 }
 
 
-static int cv_check_continuation(CVCoroState c)
+static int cv_check_continuation(CVCoroState *c)
 {
     if (c->parent == NULL) {
         return 1;
@@ -60,6 +68,7 @@ static int cv_check_continuation(CVCoroState c)
         return 0;
     }
     else {
-        return cv_check_continuation(&c->parent->state);
+        CVCoroState *p = &((c->parent).state);
+        return cv_check_continuation(p);
     }
 }
