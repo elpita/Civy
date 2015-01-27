fe897e6b8725418944178ca4bf5ec570826a0d3c
BoonsNaibot <deguzouwa@yahoo.com>
2015-01-27 12:38:26 -0600

    Update civycontinuation.c

--- civycontinuation.c
+++ civycontinuation.c
@@ -1,25 +1,18 @@
 #include "civycontinuation.h"
 
+
 struct _cvcontext {
     int state;
     void *covars;
 };
 
 
-struct _cvcontinuation {
+typedef struct _cvcontinuation {
     struct _cvcontext context;
     CVCallbackFunc *cocall;
     CVCleanupFunc *coclean;
     PyObject *coargs[3];
-};
-
-
-static void cv_dealloc_continuation(CVContinuation c)
-{
-    CVCleanupFunc cleanup = c->coclean;
-    cv_dealloc_args(c->coargs);
-    cleanup(((CVContext)c)->vars);
-}
+} CVContinuation;
 
 
 static void cv_dealloc_args(PyObject **args)
@@ -28,4 +21,16 @@ static void cv_dealloc_args(PyObject **args)
     Py_CLEAR(args[1]);
     Py_CLEAR(args[2]);
     
+}
+
+
+static void cv_dealloc_continuation(CVContinuation *c)
+{
+    CVCleanupFunc cleanup = c->coclean;
+
+    cv_dealloc_args(c->coargs);
+
+    if (cleanup != NULL) {
+        cleanup(c->context->covars);
+    }
 }
