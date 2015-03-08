01d724459f677c900d27e9c60e07a3f50611d0c8
BoonsNaibot <deguzouwa@yahoo.com>
2015-03-08 15:51:41 -0500

    Update dispatch.c

--- dispatch.c
+++ dispatch.c
@@ -1,15 +1,15 @@
 #define _is_dead(a) ((CVObject *)a)->alive
 
-int is_dead(PyObject *actor)
+static int is_dead(PyObject *actor)
 {
     PyObject *a = PyWeakref_GET_OBJECT(actor);
     return ((a == Py_None) || _is_dead(a));
 }
 
 
-void cv_control(SDL_UserEvent *event)
+static void cv_control(SDL_UserEvent *event)
 {
-    CVCoroutine *coro = (CVCoroutine *)(event->data1);
+    volatile CVCoroutine *coro = (CVCoroutine *)(event->data1);
 
     {
         PyGILState_STATE gstate = PyGILState_Ensure();
@@ -19,17 +19,26 @@ void cv_control(SDL_UserEvent *event)
             Py_XDECREF((PyObject *)(event->data2)); //Fix me
             cv_dealloc_coroutine(coro);
             PyGILState_Release(gstate);
-            return
+            return;
         }
         global_actor = PyWeakref_GET_OBJECT(state->actor_ptr);
         Py_INCREF(global_actor);
         PyGILState_Release(gstate);
     }
 
-    {
-        CVStack *stack = &(coro->stack)
-        
-        coroutine_call(stack);
+    switch(cv_setjmp(to_whatever)) {
+        case 0: {
+            CVStack *stack = &(coro->stack)
+            coroutine_call(stack);
+        }
+        case -1:
+            cv_dealloc_coroutine(coro);
+            /* Cleanup */
+        case 1: {
+            PyGILState_STATE gstate = PyGILState_Ensure();
+            Py_DECREF(global_actor);
+            PyGILState_Release(gstate);
+        }
     }
     /* Cleanup */
 }
