8581a890dbd90deafc973d980625b5dfa4b9dca0
BoonsNaibot <deguzouwa@yahoo.com>
2015-03-07 20:39:47 -0600

    Update dispatch.c

--- dispatch.c
+++ dispatch.c
@@ -2,21 +2,26 @@
 
 int is_dead(PyObject *actor)
 {
-    int i;
-    PyObject *a;
-    PyGILState_STATE gstate;
-
-    gstate = PyGILState_Ensure();
-    a = PyWeakref_GET_OBJECT(actor);
-    i = ((a == Py_None) || _is_dead(a));
-    PyGILState_Release(gstate);
-    return i;
+    PyObject *a = PyWeakref_GET_OBJECT(actor);
+    return ((a == Py_None) || _is_dead(a));
 }
 
 
 void cv_control(SDL_UserEvent *event)
 {
     CVCoroutine *coro = (CVCoroutine *)event->data1;
+
+    {
+        PyGILState_STATE gstate = PyGILState_Ensure();
+        CVCoroState *state = &coro->state; 
+
+        if (!cv_check_coroutine(state)) {
+            Py_DECREF();
+            cv_dealloc_coroutine(coro);
+        }
+        global_actor = PyWeakref_GET_OBJECT(state->actor_ptr);
+        Py_INCREF(global_actor);
+        PyGILState_Release(gstate);
+    }
     
-    if (!cv_check_coroutine(&coro->state)) {
-    
+}
