15a4e40f27cff6688bb2eb5f5357ade373e85c4f
BoonsNaibot <deguzouwa@yahoo.com>
2015-03-21 13:00:40 -0500

    Update dispatch.c

--- dispatch.c
+++ dispatch.c
@@ -12,8 +12,10 @@ static void cv_control(SDL_UserEvent *event)
     volatile CVCoroutine *coro = (CVCoroutine *)(event->data1);
 
     {
-        PyGILState_STATE gstate = PyGILState_Ensure();
+        PyGILState_STATE gstate;
         CVCoroState *state = &(coro->state); 
+
+        gstate = PyGILState_Ensure();
 
         if (!cv_check_coroutine(state)) {
             Py_XDECREF((PyObject *)(event->data2)); //Fix me
@@ -42,3 +44,21 @@ static void cv_control(SDL_UserEvent *event)
     }
     /* Cleanup */
 }
+
+
+static void coroutine_call(CVCoStack *stack)
+{
+    volatile CVContinuation *c = cv_costack_pop(stack);
+
+    global_continuation = &c;
+    switch(cv_setjmp(to_this)) {
+        case 0:
+            while (c != NULL) {
+                CVCallbackFunc cocall = c->cocall;
+                PyObject *args[3]; = c->coargs;
+
+                cocall(args[0], args[1], args[2]);
+                /* assert zero */
+                case 1: c = cv_costack_pop(stack);}
+    }
+}   
