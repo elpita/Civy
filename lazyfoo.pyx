6754fc64ed81c8b58f9078229125066c8390f9a1
BoonsNaibot <deguzouwa@yahoo.com>
2015-06-06 19:05:51 -0500

    Update lazyfoo.pyx

--- lazyfoo.pyx
+++ lazyfoo.pyx
@@ -30,11 +30,128 @@ cdef class LShaderProgram(object):
     def unbind(self):
         glUseProgram(NULL)
         
-    def print_program_log(self, program):
-        pass
+    def print_program_log(self, GLuint program):
+
+        if glIsProgram(program):
+            ### program log length ###
+            cdef int info_log_length = 0
+            cdef int max_length = info_log_length
+            
+            ### get info string length ###
+            glGetProgramiv(program, GL_INFO_LOG_LENGTH, &max_length)
+            
+            ### allocate string ###
+            cdef char *info_log = char[max_length]
+            
+            ### get info log ###
+            glGetProgramInfoLog(program, max_length, &info_log_length, info_log)
+            
+            if info_log_length > 0:
+                print "{!s}\n".format(info_log)
+        else:
+            print "name {!d} is not a program\n".format(program)
+            
         
-    def print_shader_log(self, shader):
-        pass
+    def print_shader_log(self, GLuint shader):
+        if glIsShader(shader):
+            ### shader log length ###
+            cdef int info_log_length = 0
+            cdef max_length = info_log_length
+
+            ### get info string length ###
+            glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &max_length)
+
+            ### allocate string ###
+            cdef char *info_log = char[max_length]
+
+            ### get info log ###
+            glGetShaderInfoLog(shader, max_length, &info_log_length, info_log)
+            
+            if info_log_length > 0:
+                print "{!s}\n".format(info_log)
+        else:
+            print "Name {!d} is not a shader\n".format(shader)
         
     def __dealloc__(self):
         self.free_program()
+
+
+cdef class LPlainPolygonProgram2D(LShaderProgram):
+
+    def load_program(self):
+        ### success flag ###
+        GLint program_success = GL_TRUE
+        
+        ### generate program ###
+        self.m_program_id = glCreateProgram()
+        
+        ### create vertex shader ###
+        GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER)
+        
+        ### get vertex source ###
+        const GLchar *vertex_shader_source[] = """
+                                                void main()
+                                                {
+                                                    gl_Position = gl_Vertex;
+                                                }
+                                                """
+
+        ### set vertex source ###
+        glShaderSource(vertex_shader, 1, vertex_shader_source, NULL)
+        
+        ### compile vertex source ###
+        glCompileShader(vertex_shader)
+        
+        ### check vertex shader for errors ###
+        GLint v_shader_compiled = GL_FALSE
+        glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &v_shader_compiled)
+        
+        if v_shader_compiled != GL_TRUE:
+            print "Unable to compile vertex shader {!d}!\n".format(vertex_shader)
+            self.print_shader_log(vertex_shader)
+            return False
+            
+        ### attach vertex shader to program ###
+        glAttachShader(self.m_program_id, vertex_shader)
+        
+        ### create fragment shader ###
+        GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER)
+        
+        ### get fragment source ###
+        const GLchar *fragment_shader_source[] = """
+                                                 void main{}
+                                                 {
+                                                    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+                                                 }
+                                                 """
+
+        ### set fragment source ###
+        glShaderSource(fragment_shader, 1, fragment_shader_source, NULL)
+        
+        ### compile fragment source ###
+        glCompileShader(fragment_shader)
+        
+        ### check fragment shader for errors ###
+        GLint f_shader_compiled = GL_FALSE
+        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &f_shader_compiled)
+        
+        if f_shader_compiled != GL_TRUE:
+            print "Unable to compile fragment shader {!d}!\n".format(fragment_shader)
+            self.print_shader_log(fragment_shader)
+            return False
+            
+        ### attach fragment shader to program ###
+        glAttachShader(self.m_program_id, fragment_shader)
+        
+        ### link program ###
+        glLinkProgram(self.m_program_id)
+        
+        ### check for errors ###
+        glGetProgramiv(self.m_program_id, GL_LINK_STATUS, &program_success)
+        
+        if program_success != GL_TRUE:
+            print "Error lining program {!d}!\n".format(self.m_program_id)
+            self.program_log(self.m_program_id)
+            return False
+            
+        return True
